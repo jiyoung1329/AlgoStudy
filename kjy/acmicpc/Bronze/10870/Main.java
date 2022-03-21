b8531ad16ff769ec513f113025dcafe1886ad51d
jykim <jiyoung112837@gmail.com>
2022-03-22 01:22:15 +0900

    220322 - acmicpc java update

--- kjy/acmicpc/Bronze/10870/Main.java
+++ kjy/acmicpc/Bronze/10870/Main.java
@@ -1,3 +1,20 @@
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] fibo = new int[21];
+
+        int N = Integer.parseInt(br.readLine());
+
+        fibo[0] = 0;
+        fibo[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+        System.out.println(fibo[N]);
+
+    }
 }
--- kjy/acmicpc/Silver/11729/Main.java
+++ kjy/acmicpc/Silver/11729/Main.java
@@ -1,6 +1,42 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+public class Main {
+    // 하노이탑 규칙
+    // 1. n개의 탑을 1(start)에서 3(end)로 옮기려고 한다.
+    // 2. (n-1)개 탑을 1(start)에서 2(mid)로 옮기고
+    // 3. 제일 큰판(n)을 1(start)에서 3(end)로 옮긴다.
+    // 4. (n-1)개의 탑을 2(mid)에서 3(end)로 옮긴다.
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        if (N == 1) {
+            System.out.println(1);
+        } else {
+            System.out.println((int) Math.pow(2, N) - 1);
+        }
+
+        hanoi(bw, N, 1, 3, 2);
+        bw.close();
+
+    }
+
+    public static void hanoi(BufferedWriter bw, int n, int start, int end, int mid) throws IOException {
+        if (n == 1) {
+            bw.write(start + " " + end + "\n");
+            return;
+        }
+        // n-1판을 start에서 mid로 옮기고
+        hanoi(bw, n - 1, start, mid, end);
+        // 제일 큰 판을 start에서 end로 옮기고
+        bw.write(start + " " + end + "\n");
+        // (n-1)개 판들을 mid에서 end로 옮긴다.
+        hanoi(bw, n - 1, mid, end, start);
     }
 
 }
